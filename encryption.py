84f8ae701e20df05675d41bba3296d0260fd204e
Brian Cui <brian.cui@live.com>
2018-08-29 17:52:56 -0500

    Fixed encryption bugs and committed some crypt files

--- encryption.py
+++ encryption.py
@@ -2,11 +2,11 @@ import sys
 password = "applesandoranges"
 index = 0
 
-for line in sys.stdin:
-    output = ""
-    for letter in line:
-        if letter == "\n" or letter == "\r":
-            continue
-        output = output + chr(ord(password[index]) ^ ord(letter))
-        index = (index + 1) % len(password)
-    print(output)
+plaintext = sys.stdin.read()
+output = ""
+
+for letter in plaintext:
+    output = output + chr(ord(password[index]) ^ ord(letter))
+    index = (index + 1) % len(password)
+
+sys.stdout.write(output)
